194af5700b393460e72eaade6063072f98479bd2
Donny Ostrianto Sundawa <donny.ostrianto@gmail.com>
2020-08-24 16:48:06 +0700

    change Prettier settings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
-const xml2js = require("xml2js");
-const moment = require("moment");
+const fs = require('fs');
+const xml2js = require('xml2js');
+const moment = require('moment');
 const arg = process.argv;
 
 if (!(arg && arg[2])) {
@@ -15,10 +15,8 @@ const xmlInput = fs.readFileSync(arg[2]);
       mergeAttrs: false,
     });
     const x = arg && arg[3] ? arg[3] : 12;
-    const y = arg && arg[4] ? arg[4] : "minutes";
-    const metadataTime = moment(result.gpx.metadata[0].time[0])
-      .add(x, y)
-      .toDate();
+    const y = arg && arg[4] ? arg[4] : 'minutes';
+    const metadataTime = moment(result.gpx.metadata[0].time[0]).add(x, y).toDate();
     result.gpx.metadata[0].time[0] = metadataTime.toISOString();
 
     for (let trkpt of result.gpx.trk[0].trkseg[0].trkpt) {
@@ -28,7 +26,7 @@ const xmlInput = fs.readFileSync(arg[2]);
 
     const builder = new xml2js.Builder();
     const xmlOutput = builder.buildObject(result);
-    fs.writeFileSync("result.gpx", xmlOutput);
+    fs.writeFileSync('result.gpx', xmlOutput);
   } catch (err) {
     console.log(err);
   }
